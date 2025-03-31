93faec215f6a51082b9bc44996cef5fbe23249d0
BikashSantra03 <santrabikash922@gmail.com>
2025-03-31 14:12:04 +0530

    Movies App in Next.Js

---
+++ src/utils/mocs/MovieData.tsx
@@ -0,0 +1,36 @@
+ const movies = [
+    {
+      id: "1",
+      title: "Inception",
+      comments: 124,
+      rating: 4.7,
+      description: "A mind-bending thriller by Christopher Nolan.",
+      commentsList: ["Great movie!", "Mind-blowing!", "A true masterpiece."],
+    },
+    {
+      id: "2",
+      title: "Interstellar",
+      comments: 98,
+      rating: 4.8,
+      description: "A space exploration film by Christopher Nolan.",
+      commentsList: [
+        "Amazing visual effects!",
+        "Loved the soundtrack.",
+        "A bit confusing but great.",
+      ],
+    },
+    {
+      id: "3",
+      title: "The Dark Knight",
+      comments: 255,
+      rating: 4.9,
+      description: "The iconic Batman film directed by Christopher Nolan.",
+      commentsList: [
+        "The best Batman movie!",
+        "Heath Ledger was outstanding.",
+        "Iconic movie.",
+      ],
+    },
+  ];
+
+export default movies;
