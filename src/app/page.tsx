51d660219f5a84a6ae62b280211fbd751f9fc834
BikashSantra03 <santrabikash922@gmail.com>
2025-04-05 20:19:04 +0530

    GET Method

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,20 +3,7 @@ import React from "react";
 import KingFisherImage from "../../public/KingFsiher Image.jpg";
 
 const page = () => {
-  return (
-    <>
-      <div>Home page</div>
-
-      <Image src={KingFisherImage} alt="KingFisherImage" />
-
-      <Image
-        src="https://cdn.britannica.com/35/3635-050-96241EC1/Scarlet-macaw-ara-macao.jpg"
-        alt="bird-img"
-        width={300}
-        height={300}
-      />
-    </>
-  );
+  return <div>Home page</div>;
 };
 
 export default page;
