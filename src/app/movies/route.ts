a7e62d6fdafcbe734ee2c322c99fe6b95d58cbd0
BikashSantra03 <santrabikash922@gmail.com>
2025-04-05 20:20:00 +0530

    POST Method

---
+++ src/app/movies/route.ts
@@ -0,0 +1,13 @@
+import { movies } from "./db";
+
+export async function GET() {
+  return Response.json(movies);
+}
+
+export async function POST(req: Request) {
+  const movie = await req.json();
+
+  const newMovie = { ...movie };
+  movies.push(newMovie);
+  return new Response(JSON.stringify(newMovie));
+}
