12b14fc119ce0d1bd6859b6e1c31a136ba8bbe9c
BikashSantra03 <santrabikash922@gmail.com>
2025-04-05 08:17:24 +0530

    redirect

--- src/app/analytics/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Analytics = () => {
-  return (
-    <div>Analytics page</div>
-  )
-}
-
-export default Analytics
--- src/app/analytics/revenue/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Revenue = () => {
-  return (
-    <div>Revenue page</div>
-  )
-}
-
-export default Revenue
