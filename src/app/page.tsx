276d6bcc221bb54e8b4fb089177d0a40824920de
BikashSantra03 <santrabikash922@gmail.com>
2025-04-05 09:22:35 +0530

    Image optimizing in Next.js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import KingFisherImage from "../../public/KingFsiher Image.jpg";
 
 const page = () => {
   return (
-    <div>Home page</div>
-  )
-}
+    <>
+      <div>Home page</div>
 
-export default page
+      <Image src={KingFisherImage} alt="KingFisherImage" />
+
+      <Image
+        src="https://cdn.britannica.com/35/3635-050-96241EC1/Scarlet-macaw-ara-macao.jpg"
+        alt="bird-img"
+        width={300}
+        height={300}
+      />
+    </>
+  );
+};
+
+export default page;
--- src/app/users/page.tsx
+++
@@ -1,8 +0,0 @@
-const Users = async () => {
-  if (2 > 1) {
-    throw new Error("Error in the dashboard");
-  }
-  return <h1>Users Page</h1>;
-};
-
-export default Users;
