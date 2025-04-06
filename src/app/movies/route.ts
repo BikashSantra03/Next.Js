6a80a80a743da3d97663d338d82978b6632b8427
BikashSantra03 <santrabikash922@gmail.com>
2025-04-06 14:20:24 +0530

    NextRequest, nextUrl, searchParams in Next.js

--- src/app/movies/route.ts
+++ src/app/movies/route.ts
@@ -1,5 +1,14 @@
+import { NextRequest } from "next/server";
 import { movies } from "./db";
 
-export async function GET() {
-  return new Response(JSON.stringify(movies));
+export async function GET(request: NextRequest) {
+  const url = request.nextUrl;
+  const searchParams = url.searchParams;
+  const query = searchParams.get("query");
+
+  const filteredMovies = query
+    ? movies.filter((m) => m.name.toLowerCase().includes(query.toLowerCase()))
+    : movies;
+
+  return new Response(JSON.stringify(filteredMovies));
 }
