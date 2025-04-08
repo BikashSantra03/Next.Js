4c00b69cc04385d90e2ab822fb6a855897048fad
BikashSantra03 <santrabikash922@gmail.com>
2025-04-08 06:59:47 +0530

    Add new feature in the 'new-feature' folder

--- src/app/@comments/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const comments = async () => {
-  return (
-    <div className="border border-black w-[300px] h-[300px]">comments</div>
-  );
-};
-
-export default comments;
